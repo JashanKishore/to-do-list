a2a60bc4a4f3d71e65980d61d306eb5777e994d5
Jashan <jkis0002@student.monash.edu>
2024-01-14 22:54:00 +1100

    Added tab bar for different categories of tasks

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,18 +12,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-
-      appBar: AppBar(
-        title: Text('TO DO',
-        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-      ),
-      elevation: 0,
-      backgroundColor: Colors.black,
-      shadowColor: Colors.white,
-      shape: Border(
-          bottom: BorderSide(color: Colors.white, width: 1.0), // Set the border width
+    return DefaultTabController(
+      length: 3, // Number of tabs
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text(
+            'TO DO',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          elevation: 0,
+          backgroundColor: Colors.black,
+          shadowColor: Colors.white,
+          shape: Border(
+            bottom: BorderSide(color: Colors.white, width: 2.0),
+          ),
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Pending'),
+              Tab(text: 'Completed'),
+              Tab(text: 'Overdue'),
+            ],
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.grey,
+            indicatorColor: Colors.white,
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            // Widgets for the 'Pending' tab
+            Center(child: Text('Pending Tasks')),
+            // Widgets for the 'Completed' tab
+            Center(child: Text('Completed Tasks')),
+            // Widgets for the 'Overdue' tab
+            Center(child: Text('Overdue Tasks')),
+          ],
         ),
       ),
     );
